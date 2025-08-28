bd852f4e59cd98c6eca68735305473897cc68f7c
Daniel Beckman <Dbeckman3@hu8skers.unl.edu>
2025-08-28 12:48:55 -0500

    initial commit

---
+++ name_print.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Daniel Beckman
+ *
+ * Date 2025/28/08
+ *
+ * A name printing program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Daniel Beckman\n");
+
+    return 0;
+}
